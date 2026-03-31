6396680b572e316ca5ece9e9fb801d934ff2bd06
“Justedizi” <krzysztof99c@gmail.com>
2026-03-31 03:42:42 +0200

    done one

---
+++ 900/multiply_by_2_6.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    int div_3 = 0;
+    int div_2 = 0;
+    while (n > 0 && n % 3 == 0) {
+      div_3++;
+    }
+    while (n > 0 && n % 2 == 0) {
+      div_2++;
+    }
+    if (n > 1 || div_2 > div_3)
+      cout << -1 << '\n';
+    else
+      cout << div_3 + (div_3 - div_2) << '\n';
+  }
+}
