573d06c868f6f02956b3c7c53ae81ac2f61d5bd4
“Justedizi” <krzysztof99c@gmail.com>
2026-06-01 23:09:35 +0200

    im comming back

---
+++ 1100/250k_tons_of_dmt.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<long long> a(n);
+    vector<long long> pref(n + 1, 0);
+    for (int i = 0; i < n; i++) {
+      cin >> a[i];
+      pref[i + 1] = pref[i] + a[i];
+    };
+    long long res = 0;
+    for (int div = 1; div <= n; div++) {
+      long long min_pref = 1e18;
+      long long max_pref = -1;
+      if (n % div == 0) {
+        for (int i = 0; i < n; i += div) {
+          long long block = pref[i + div] - pref[i];
+          min_pref = min(min_pref, block);
+          max_pref = max(max_pref, block);
+        }
+      }
+      res = max(res, max_pref - min_pref);
+    }
+    cout << res << '\n';
+  }
+  return 0;
+}
